7892c7827c445355a1a81026d6ecd5fb490331d0
= <=>
2022-03-04 00:21:04 -0300

    refactor: Simplify code

--- src/main.py
+++ src/main.py
@@ -1,10 +1,10 @@
 print("--main.py")
 from machine import SPI, Pin
 from rgb import color565
-import ili9341
+from ili9341 import ILI9341
 
 
 spi = SPI(1, baudrate=32000000)
-display = ili9341.ILI9341(spi, cs=Pin(0), dc=Pin(15))
+display = ILI9341(spi, cs=Pin(0), dc=Pin(15))
 
 # display.fill(color565(255, 0, 0))
