64121987883795c675ef504ca80bef354f2c4f6e
= <=>
2022-03-04 00:19:46 -0300

    refactor: Simplify code

--- src/lib/rgb.py
+++ src/lib/rgb.py
@@ -2,9 +2,15 @@ import utime
 import ustruct
 
 
-def color565(r, g, b):
-    return (r & 0xf8) << 8 | (g & 0xfc) << 3 | b >> 3
-
+def color565(r, g=0, b=0):
+    """Convert red, green and blue values (0-255) into a 16-bit 565 encoding.  As
+    a convenience this is also available in the parent adafruit_rgb_display
+    package namespace."""
+    try:
+        r, g, b = r  # see if the first var is a tuple/list
+    except TypeError:
+        pass
+    return (r & 0xF8) << 8 | (g & 0xFC) << 3 | b >> 3
 
 class DummyPin:
     """A fake gpio pin for when you want to skip pins."""
